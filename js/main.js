c898ba474814f83d40f09ece4632c579a4fa7301
OverlordCrowley <maksim2004@bk.ru>
2021-08-07 01:03:09 +0600

    Revert "add: added hover effects"
    This reverts commit e0a369e0a07c4cd796a81a5bb0e099f79d7b58bb.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var tabsItem = $('.recommendation-list__item');
-  tabsItem.on('click', function(event){
-    event.preverntDefault();
-    tabsItem.removeClass("recommendation-list__item-active");
-    $(this).addClass("recommendation-list__item-active");
-  });
-});
